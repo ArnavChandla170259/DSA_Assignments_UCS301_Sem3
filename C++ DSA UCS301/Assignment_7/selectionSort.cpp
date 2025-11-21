71fe1fa025ca7c455bdcb497811f44299d2abc91
Arnav Chandla <156535882+ArnavChandla170259@users.noreply.github.com>
2025-11-21 16:22:20 +0530

    Implement selection sort algorithm in selectionSort.cpp

--- C++ DSA UCS301/Assignment_7/selectionSort.cpp
+++ C++ DSA UCS301/Assignment_7/selectionSort.cpp
@@ -1,18 +1,23 @@
 #include<iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n){
-	for(int i=0;i<n-1;i++){
+void selectionSort(int arr[], int n)
+{
+	for(int i=0;i<n-1;i++)
+	{
 		int minIndex = i;
-		for(int j=i+1;j<n;j++){
-			if(arr[j]<arr[minIndex]){
+		for(int j=i+1;j<n;j++)
+		{
+			if(arr[j]<arr[minIndex])
+			{
 				minIndex = j;
 			}
 		}
 		swap(arr[i],arr[minIndex]);
 	}
 }
-int main(){
+int main()
+{
 	int arr[] = {11, 10, 8, 15,20};
 	int n =5;
 	selectionSort(arr,n);
@@ -20,7 +25,7 @@ int main(){
 	for(int i=0;i<n;i++){
 		cout << arr[i] << " ";
 	}	
-	return 0;
+		return 0;
 }
 /* Name - Arnav Chandla 
 Roll no. - 1024170259*/
