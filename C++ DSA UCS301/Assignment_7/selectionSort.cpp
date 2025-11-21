a1ba68111e6b3ac1c860cda008c4fd9224fa0f4a
Arnav Chandla <156535882+ArnavChandla170259@users.noreply.github.com>
2025-11-21 16:00:48 +0530

    Update and rename Q1.cpp to selectionSort.cpp

---
+++ C++ DSA UCS301/Assignment_7/selectionSort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n){
+	for(int i=0;i<n-1;i++){
+		int minIndex = i;
+		for(int j=i+1;j<n;j++){
+			if(arr[j]<arr[minIndex]){
+				minIndex = j;
+			}
+		}
+		swap(arr[i],arr[minIndex]);
+	}
+	
+}
+
+int main(){
+	int arr[] = {11, 10, 8, 15,20};
+	int n =5;
+	selectionSort(arr,n);
+	cout << "Sorted Array is: ";
+	for(int i=0;i<n;i++){
+		cout << arr[i] << " ";
+	}	
+	return 0;
+}
+/* Name - Arnav Chandla 
+Roll no. - 1024170259*/
